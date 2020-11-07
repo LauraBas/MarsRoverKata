45af3e6b7800fd75e12d9ce8cd0d44fb20723c7c
Laura Bassani <bassanilaurarita@gmail.com>
2020-11-07 12:22:09 +0100

    Test return rover input, initial position, plateau dimensions and instructions

--- src/Models/Rover.php
+++ src/Models/Rover.php
@@ -3,14 +3,46 @@
 namespace App\Models;
 
 Class Rover {
-
+    
     public function __construct(string $input)
     {
         $this->input = $input;
+        $roverInfo;
+        $initialPosition;
+        $instructions;
+        $plateauDimensions;
+                   
     }
 
     public function getInput() :string
     {
         return $this->input;
     }
+
+    public function getPlateauDimensions() :string
+    {
+        $input = $this->getInput();
+        $roverInfo = explode("\n", $input);
+        $this->plateauDimensions = $roverInfo[0];
+        return $this->plateauDimensions;
+    }
+   
+    public function getInitialPosition() :string 
+    {
+        $input = $this->getInput();
+        $roverInfo = explode("\n", $input);
+        $this->initialPosition = $roverInfo[1];
+        return $this->initialPosition;
+    }
+
+    public function getInstructions() :string 
+    {
+        $input = $this->getInput();
+        $roverInfo = explode("\n", $input);
+        $this->instructions = $roverInfo[2];
+        return $this->instructions;
+
+    }
+    
+    
 }
--- tests/RoverTest.php
+++ tests/RoverTest.php
@@ -10,14 +10,43 @@ class RoverTest extends TestCase
 
 	public function test_return_rover_input()
 	{
-        //given
+        
 		$rover = new Rover(
-            "5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM");
+            "5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM");        
         
-        //when
-		$rover->getInput();
+		$rover->getInput();        
         
-        //then
 		$this->assertEquals("5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM", $rover->getInput());
-    }
+	}
+	public function test_return_plateau_dimensions()
+	{
+        
+		$rover = new Rover(
+            "5 5\n1 2 N\nLMLMLMLMM");        
+        
+		$result= $rover->getPlateauDimensions();      
+        
+		$this->assertEquals('5 5', $result);
+	}
+    public function test_return_rover_initial_position()
+	{
+        
+		$rover = new Rover(
+            "5 5\n1 2 N\nLMLMLMLMM");        
+        
+		$result= $rover->getInitialPosition();      
+        
+		$this->assertEquals('1 2 N', $result);
+	}
+	
+	public function test_return_rover_instructions()
+	{
+        
+		$rover = new Rover(
+            "5 5\n1 2 N\nLMLMLMLMM");        
+        
+		$result= $rover->getInstructions();      
+        
+		$this->assertEquals('LMLMLMLMM', $result);
+	}
 }
